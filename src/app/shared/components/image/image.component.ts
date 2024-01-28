8f6e5ce9fa1e6628e636d756a9d19b53a4cff4e4
Arnaud Echavidre <a.echavidre@gmail.com>
2024-01-28 17:06:04 +0100

    [feat]: add ngOptimized directive

--- src/app/shared/components/image/image.component.ts
+++ src/app/shared/components/image/image.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-image',
   standalone: true,
   templateUrl: './image.component.html',
+  imports: [
+    NgOptimizedImage
+  ],
   styleUrl: './image.component.scss'
 })
 export class ImageComponent {
